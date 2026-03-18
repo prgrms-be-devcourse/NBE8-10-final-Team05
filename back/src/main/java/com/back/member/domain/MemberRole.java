3ac3ac58f154103c775cb9f6a51b7d6127125d1a
AquilaXk <illusiveman7@gmail.com>
2026-03-18 15:13:11 +0900

    Feat : #2 MemberRole, MemberStatus 추가

---
+++ back/src/main/java/com/back/member/domain/MemberRole.java
@@ -0,0 +1,7 @@
+package com.back.member.domain;
+
+/** 회원 권한 구분(USER: 일반 회원, ADMIN: 관리자). */
+public enum MemberRole {
+  USER,
+  ADMIN
+}
---
+++ back/src/main/java/com/back/member/domain/MemberStatus.java
@@ -0,0 +1,8 @@
+package com.back.member.domain;
+
+/** 회원 상태(ACTIVE: 정상, BLOCKED: 운영 차단, WITHDRAWN: 탈퇴). */
+public enum MemberStatus {
+  ACTIVE,
+  BLOCKED,
+  WITHDRAWN
+}
