b4fa4bec5fdbb7cda3ed36c3f71a8392ad2b6d79
장인호 <jih95349238@gmail.com>
2026-03-19 16:17:57 +0900

    Feat:14 Letter 받은 편지, 보낸 편지 조회 페이징 도입

--- back/src/main/java/com/back/letter/dto/LetterListRes.java
+++ back/src/main/java/com/back/letter/dto/LetterListRes.java
@@ -1,8 +1,25 @@
 package com.back.letter.dto;
 
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 
 public record LetterListRes(
-    List<LetterItem> letters
+    List<LetterItem> letters,
+    int totalPages,
+    long totalElements,
+    int currentPage,
+    boolean isFirst,
+    boolean isLast
 ) {
+    public static LetterListRes from(Page<LetterItem> page){
+        return new LetterListRes(
+                page.getContent(),
+                page.getTotalPages(),
+                page.getTotalElements(),
+                page.getNumber(),
+                page.isFirst(),
+                page.isLast()
+        );
+    }
 }
