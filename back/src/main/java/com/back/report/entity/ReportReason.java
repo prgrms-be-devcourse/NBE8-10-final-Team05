9e7dfaddfe0c056adc0d6827973d790c4f36ced8
chaeun12 <ic366632@gmail.com>
2026-03-23 16:20:42 +0900

    feat: #27 신고 사유, 상태, 대상 타입 정의

---
+++ back/src/main/java/com/back/report/entity/ReportReason.java
@@ -0,0 +1,16 @@
+package com.back.report.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReportReason {
+    PROFANITY("욕설 및 비방"),
+    SPAM("스팸 및 광고"),
+    INAPPROPRIATE("부적절한 내용"),
+    PERSONAL_INFO("개인정보 노출"),
+    OTHER("기타");
+
+    private final String content;
+}
