93dc5d6537e7ee34f8623c8a072b7d2c6f4add38
chaeun12 <ic366632@gmail.com>
2026-03-27 10:39:44 +0900

    feat: #77 동일 대상의 중복 신고 조회를 위한 쿼리 메서드 추가

--- back/src/main/java/com/back/report/repository/ReportRepository.java
+++ back/src/main/java/com/back/report/repository/ReportRepository.java
@@ -1,9 +1,12 @@
 package com.back.report.repository;
 
 import com.back.report.entity.Report;
+import com.back.report.entity.ReportStatus;
 import com.back.report.entity.TargetType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
     boolean existsByReporterIdAndTargetIdAndTargetType(Long reporterId, Long targetId, TargetType targetType);
-}
+    List<Report> findAllByTargetTypeAndTargetIdAndStatus(TargetType targetType, Long targetId, ReportStatus status);}
