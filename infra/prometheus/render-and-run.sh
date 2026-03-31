bf69201381728936498527d84ab053cf3d5d6250
AquilaXk <illusiveman7@gmail.com>
2026-03-31 12:21:03 +0900

    feat:#96 docker-compose 통합 및 모니터링 경로 정리

---
+++ infra/prometheus/render-and-run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+TEMPLATE_PATH="/etc/prometheus/prometheus.yml.tmpl"
+RENDERED_PATH="/tmp/prometheus.yml"
+
+: "${BACKEND_SCRAPE_TARGET:=maum-on-back:8080}"
+
+sed "s#__BACKEND_SCRAPE_TARGET__#${BACKEND_SCRAPE_TARGET}#g" "$TEMPLATE_PATH" > "$RENDERED_PATH"
+
+exec /bin/prometheus \
+  --config.file="$RENDERED_PATH" \
+  --storage.tsdb.path=/prometheus \
+  --web.enable-lifecycle \
+  --web.enable-remote-write-receiver \
+  --web.external-url=http://localhost:3400/prometheus/ \
+  --web.route-prefix=/
