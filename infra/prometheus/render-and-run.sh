8b43ab684417467550b0587fe3f4984e414506a9
AquilaXk <illusiveman7@gmail.com>
2026-04-03 10:11:53 +0900

    chore(front):#133 backend-monitoring 분리

--- infra/prometheus/render-and-run.sh
+++ infra/prometheus/render-and-run.sh
@@ -5,6 +5,7 @@ TEMPLATE_PATH="/etc/prometheus/prometheus.yml.tmpl"
 RENDERED_PATH="/tmp/prometheus.yml"
 
 : "${BACKEND_SCRAPE_TARGET:=maum-on-back:8080}"
+: "${PROMETHEUS_EXTERNAL_URL:=http://localhost:3400/prometheus/}"
 
 sed "s#__BACKEND_SCRAPE_TARGET__#${BACKEND_SCRAPE_TARGET}#g" "$TEMPLATE_PATH" > "$RENDERED_PATH"
 
@@ -13,5 +14,5 @@ exec /bin/prometheus \
   --storage.tsdb.path=/prometheus \
   --web.enable-lifecycle \
   --web.enable-remote-write-receiver \
-  --web.external-url=http://localhost:3400/prometheus/ \
+  --web.external-url="$PROMETHEUS_EXTERNAL_URL" \
   --web.route-prefix=/
